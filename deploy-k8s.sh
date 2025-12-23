6851369cc257629342c4f40cccd4e8f86ad6aa02
secant78 <scanny@udel.edu>
2025-12-23 02:22:22 -0500

    added helm and installed grafana, loki, prometheus

--- deploy-k8s.sh
+++ deploy-k8s.sh
@@ -30,3 +30,24 @@ kubectl apply -f k8s/50-ingress.yaml
 
 echo "Deployment Complete! Checking status..."
 kubectl get all -n k8s-assessment
+
+# 9. Monitoring and Logging Setup with Helm
+echo "Setting up Monitoring and Logging with Helm..."
+# 1. Add the Helm Repositories
+./helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+./helm repo add grafana https://grafana.github.io/helm-charts
+./helm repo update
+
+# 2. Install Metrics Stack (Prometheus + Grafana)
+echo "Installing Prometheus & Grafana..."
+./helm install monitoring prometheus-community/kube-prometheus-stack \
+  --namespace monitoring --create-namespace \
+  --set grafana.adminPassword="admin" \
+  --set prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues=false
+
+# 3. Install Logs Stack (Loki + Promtail)
+echo "Installing Loki (Logs)..."
+./helm install loki grafana/loki-stack \
+  --namespace monitoring \
+  --set grafana.enabled=false \
+  --set promtail.enabled=true
